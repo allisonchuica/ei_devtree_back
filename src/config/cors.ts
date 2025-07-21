bef70780787312662244c952309a5249ab989811
allisonchuica <allisonchuicamontoya@gmail.com>
2025-07-21 00:19:45 -0500

    cambio de variable para URLs v2

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,9 +1,9 @@
 import { CorsOptions } from 'cors';
 
-const VITE_API_URL = process.env.VITE_API_URL;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 export const corsConfig: CorsOptions = {
-  origin: [FRONTEND_URL, VITE_API_URL], // frontend y postman
+  //origin: [FRONTEND_URL, VITE_API_URL], // frontend y postman
+  origin: FRONTEND_URL,
   credentials: true
 };
